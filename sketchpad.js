228055d90e0175326ab839d792174c79ff9c5147
Atish Patel <atish.patel95@yahoo.com>
2016-12-23 17:11:42 -0500

    sketchpad 1.0

--- sketchpad.js
+++ sketchpad.js
@@ -1,8 +1,9 @@
-
+var size = 48;
+var color;
 
 
 $(document).ready(function() {
-	var size = 93;
+	
 	for (var i = 0; i < size; i++){
 		for(var j = 0; j < size; j++){
 			$('#container').append('<div class="box"> </div>');
@@ -13,4 +14,28 @@ $(document).ready(function() {
 	$('.box').hover(function(){
 		$(this).addClass('marked');
 	});
+
 });
+
+var restart = function(){
+
+	$(document).ready(function(){
+		$('.marked').removeClass('marked');
+	});
+
+}
+
+/*function changeColor(var color){
+
+	$(document).ready(function(){
+		$('.box').hover(function(){
+			if (color == 'blue'){
+				$(this).addClass('blue');
+			} else if (color == 'green'){
+				$(this).addClass('green');
+			} else if (color == 'red'){
+				$(this).addClass('red');
+			}
+		});
+	});
+}*/
